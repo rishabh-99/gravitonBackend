d6d8139744be7e1a0651daa45137b75b7b69b39a
Rishabh <mehrotra.rishab@gmail.com>
2020-09-12 11:54:04 +0530

    jwt expiry set to 2 min

--- api/services/auth.service.js
+++ api/services/auth.service.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : '78uighjfyuyu97puoiohgf879';
 
 const authService = () => {
-  const issue = (payload) => jwt.sign(payload, secret, { expiresIn: 10800 });
+  const issue = (payload) => jwt.sign(payload, secret, { expiresIn: 120 });
   const verify = (token, cb) => jwt.verify(token, secret, {}, cb);
 
   return {
