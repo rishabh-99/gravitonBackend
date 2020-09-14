8fdcd9c7d7de97575e43ca87b484600bb9a8b392
Rishabh <mehrotra.rishab@gmail.com>
2020-09-14 14:21:57 +0530

    new try

--- api/services/auth.service.js
+++ api/services/auth.service.js
@@ -5,15 +5,26 @@ const CryptoJS = require("crypto-js");
 const jwtSecret = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : '78uighjfyuyu97puoiohgf879';
 const encryptSecret = process.env.NODE_ENV === 'production' ? process.env.ENCRYPT_SECRET : 'b14ca5898a4e4133bbce2ea2315a1916';
 
-var iv = CryptoJS.enc.Utf8.parse('b14ca5898a4e4133'); 
+var key = CryptoJS.enc.Utf8.parse('8080808080808080');  
+var iv = CryptoJS.enc.Utf8.parse('8080808080808080'); 
 const authService = () => {
   const issue = (payload) => {
     console.log('AAA')
-    const encryptedPayload = CryptoJS.AES.encrypt(JSON.stringify(payload), encryptSecret, { padding: CryptoJS.pad.Pkcs7 , mode: CryptoJS.mode.CBC, iv:iv }).toString()
+    const encryptedPayload = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(JSON.stringify(payload)), key, {  
+      keySize: 128 / 8,  
+      iv: iv,  
+      mode: CryptoJS.mode.CBC,  
+      padding: CryptoJS.pad.Pkcs7  
+  }).toString();
     console.log('AAA')
-    const dec = CryptoJS.AES.decrypt(encryptedPayload, encryptSecret, { padding: CryptoJS.pad.Pkcs7 , mode: CryptoJS.mode.CBC, iv:iv }).toString(CryptoJS.enc.Utf8);
+    const dec = CryptoJS.AES.decrypt(encryptedPayload, key,{  
+      keySize: 128 / 8,  
+      iv: iv,  
+      mode: CryptoJS.mode.CBC,  
+      padding: CryptoJS.pad.Pkcs7  
+  }).toString(CryptoJS.enc.Utf8);
     console.log(dec)
-    return jwt.sign({encryptedPayload}, jwtSecret, {
+    return jwt.sign({encryptedPayload: encryptedPayload}, jwtSecret, {
       expiresIn: 120
     })
 
