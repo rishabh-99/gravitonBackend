280cf2b13ecefacc7e0f15cdde4561393f5dc036
Rishabh <mehrotra.rishab@gmail.com>
2020-09-14 13:08:24 +0530

    key changeed to 32 bits key

--- api/services/auth.service.js
+++ api/services/auth.service.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
 const CryptoJS = require("crypto-js");
 
+
 const jwtSecret = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : '78uighjfyuyu97puoiohgf879';
-const encryptSecret = process.env.NODE_ENV === 'production' ? process.env.ENCRYPT_SECRET : 'd984urifjksdcasofdhasafae';
+const encryptSecret = process.env.NODE_ENV === 'production' ? process.env.ENCRYPT_SECRET : 'b14ca5898a4e4133bbce2ea2315a1916';
 
 const authService = () => {
   const issue = (payload) => {
